593875d20829c95ea0862f7b935e3ff6878faded
Junior Tavarez <jtavarez8@gmail.com>
2020-02-12 14:23:01 -0500

    display order now button

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -11,6 +11,18 @@ class Product {
     }
 }
 
+class ShoppingCart{
+    item = []
+    render(){
+        const cartEl = document.createElement('section')
+        cartEl.innerHTML = `
+        <h2>Total: \$${0}</h2>
+        <button>Order Now!</button>
+        `    
+        cartEl.className = 'cart'
+        return cartEl
+    }
+}
 class ProductItem {
     constructor (product){
         this.product = product
@@ -61,7 +73,7 @@ class ProductList{
     constructor(){}
 
     render() {
-        const renderHook = document.getElementById('app')
+        
         const prodList = document.createElement('ul')
         prodList.className = 'product-list'
             for (const prod of this.products){
@@ -69,9 +81,22 @@ class ProductList{
             const prodEl = productItem.render()
             prodList.append(prodEl)
             }
-        renderHook.append(prodList)
+            return prodList
+        }
+    }
+
+    class Shop {
+        render(){
+            const renderHook = document.getElementById('app')
+            const cart = new ShoppingCart()
+            const cartEl = cart.render()
+            const productList = new ProductList()
+            const prodListEl = productList.render()
+
+            renderHook.append(cartEl)
+            renderHook.append(prodListEl)
         }
     }
         
-const productList = new ProductList()
-productList.render()
+const shop = new Shop()
+shop.render()
