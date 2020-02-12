3819be46fc912e884ddfd20b0e1e68a483ceeb02
Junior Tavarez <jtavarez8@gmail.com>
2020-02-12 09:41:36 -0500

    starting to work with oop

--- objects-05-dynamic-access-and-assignment/assets/scripts/objects.js
+++ objects-05-dynamic-access-and-assignment/assets/scripts/objects.js
@@ -1,31 +1,75 @@
-const movieList = document.getElementById('movie-list');
+const addMovieBtn = document.getElementById('add-movie-btn')
+const searchBtn = document.getElementById('search-btn')
 
-movieList.style['background-color'] = 'red';
-movieList.style.display = 'block';
+const movies = []
 
-const userChosenKeyName = 'level';
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list')
+  
+  
+  if (movies.length === 0 ){
+    movieList.classList.remove('visible')
+    return
+  } else {
+    movieList.classList.add('visible')
+  }
+  movieList.innerHTML = ''
 
-let person = {
-  'first name': 'Max',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  [userChosenKeyName]: '...',
-  greet: function() {
-    alert('Hi there!');
-  },
-  1.5: 'hello'
-};
+  const filteredMovies = !filter 
+    ? movies
+    : movies.filter(movie => movie.info.title.includes(filter) )
 
-// ...
+    filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement('li')
+    if ('info' in movie){
 
-// person.age = 31;
-delete person.age;
-// person.age = undefined;
-// person.age = null;
-person.isAdmin = true;
+    }
+    const { info, ...otherProps } = movie;
+    console.log(otherProps)
+    // const { title: movietitle }= info;
+    let { getFormattedtitle } = movie
+    // getFormattedtitle = getFormattedtitle.bind(movie) 
+    let text = getFormattedtitle.call(movie) + ' - '
+    for (const key in info){
+      if (key !== 'title'){
+        text = text + `${key}: ${info[key]}`
+      }
+    }
+    movieEl.textContent = text
+    movieList.append(movieEl)
+  })
+}
 
-const keyName = 'first name';
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value
+  const extraName = document.getElementById('extra-name').value
+  const extraValue = document.getElementById('extra-value').value
 
-console.log(person[keyName]);
-console.log(person[1.5]);
-console.log(person);
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+    ) {
+      return 
+    }
+    const newMovie = {
+      info: {
+        title,
+        [extraName]: extraValue
+      },
+      id: Math.random().toString(),
+      getFormattedtitle(){
+        return this.info.title.toUpperCase()
+      }
+    }
+    movies.push(newMovie);
+    renderMovies()
+  }
+
+  const searchMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value
+    renderMovies(filterTerm)
+  }
+
+  addMovieBtn.addEventListener('click', addMovieHandler)
+  searchBtn.addEventListener('click', searchMovieHandler)
