3d5d29f9fdf0bbd0317a5d2adbf53271eb6e5864
Junior Tavarez <jtavarez8@gmail.com>
2020-02-12 13:56:45 -0500

    add to cart button  working

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,41 +1,77 @@
-const productList = {
-    products: [
-        {
-            title: 'A Pillow',
-            imageUrl : 'https://www.shopmarriott.com/images/products/v2/lrg/Marriott-The-Marriott-Pillow-MAR-108-L_lrg.jpg', 
-            price: 19.99,
-            description: 'A soft pillow'
-        },
-        {
-            title: 'A Car',
-            imageUrl : 'https://c.ndtvimg.com/2019-08/k8519lf8_bugatti-centodieci-unveiled-at-pebble-beach-car-show_625x300_17_August_19.jpg', 
-            price: 20000.99,
-            description: 'Expensive Car'
-        }
-    ],
-    render() {
-    const renderHook = document.getElementById('app')
-    const prodList = document.createElement('ul')
-    prodList.className = 'product-list'
-        for (const prod of this.products){
-            const prodEl = document.createElement('li')
-            prodEl.className = 'product-item'
-            prodEl.innerHTML = `
-                <div>
-                <img src="${prod.imageUrl}" alt="${prod.title}">
-                <div class="product-item__content">
-                <h2>${prod.title}</h2>
-                <h3>\$${prod.price}</h3>
-                <p>${prod.description}</p>
-                <button>Add to Cart</button>
-                </div>
-                </div>
-                `
-        prodList.append(prodEl)
-            }
+class Product {
+    title = 'DEFAULT'
+    imageUrl
+    description
+    price
+    constructor(title, image, desc, price){
+        this.title = title
+        this.imageUrl = image
+        this.description = desc
+        this.price = price
+    }
+}
 
-    renderHook.append(prodList)
+class ProductItem {
+    constructor (product){
+        this.product = product
+       
+    }
+
+    addToCart() {
+        console.log('adding product to cart...')
+        console.log(this.product)
+    }
+
+    render (){
+        const prodEl = document.createElement('li')
+        prodEl.className = 'product-item'
+        prodEl.innerHTML = `
+            <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}">
+            <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+            </div>
+            </div>
+            `
+            const addCartButton = prodEl.querySelector('button')
+            addCartButton.addEventListener('click', this.addToCart.bind(this))
+            return prodEl
     }
     
 }
+
+class ProductList{
+    products = [
+        new Product(
+            'A Pillow',
+            'https://www.shopmarriott.com/images/products/v2/lrg/Marriott-The-Marriott-Pillow-MAR-108-L_lrg.jpg',
+            'A soft pillow',
+            19.99
+              ),
+        new Product(
+            'Car',
+            'https://c.ndtvimg.com/2019-08/k8519lf8_bugatti-centodieci-unveiled-at-pebble-beach-car-show_625x300_17_August_19.jpg',
+            'Expensive Car',
+            20,000
+        )
+    ]
+    constructor(){}
+
+    render() {
+        const renderHook = document.getElementById('app')
+        const prodList = document.createElement('ul')
+        prodList.className = 'product-list'
+            for (const prod of this.products){
+            const productItem = new ProductItem(prod)
+            const prodEl = productItem.render()
+            prodList.append(prodEl)
+            }
+        renderHook.append(prodList)
+        }
+    }
+        
+const productList = new ProductList()
 productList.render()
