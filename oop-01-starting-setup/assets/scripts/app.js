86adc03d6985f35e784153b390193a041078c575
Junior Tavarez <jtavarez8@gmail.com>
2020-02-12 22:03:12 -0500

    working with CLASS

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,43 +1,85 @@
 class Product {
-    title = 'DEFAULT'
-    imageUrl
-    description
-    price
-    constructor(title, image, desc, price){
-        this.title = title
-        this.imageUrl = image
-        this.description = desc
-        this.price = price
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+class Component {
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
+  }
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
     }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
 }
 
-class ShoppingCart{
-    item = []
-    render(){
-        const cartEl = document.createElement('section')
-        cartEl.innerHTML = `
+class ShoppingCart extends Component {
+  items = [];
+
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
+
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+  render() {
+    const cartEl = this.createRootElement('section', 'cart');
+    cartEl.innerHTML = `
         <h2>Total: \$${0}</h2>
         <button>Order Now!</button>
-        `    
-        cartEl.className = 'cart'
-        return cartEl
-    }
+        `;
+    this.totalOutput = cartEl.querySelector('h2');
+  }
 }
-class ProductItem {
-    constructor (product){
-        this.product = product
-       
-    }
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId);
+    this.product = product;
+  }
 
-    addToCart() {
-        console.log('adding product to cart...')
-        console.log(this.product)
-    }
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
 
-    render (){
-        const prodEl = document.createElement('li')
-        prodEl.className = 'product-item'
-        prodEl.innerHTML = `
+  render() {
+    const prodEl = this.createRootElement('li', 'product-item');
+    prodEl.innerHTML = `
             <div>
             <img src="${this.product.imageUrl}" alt="${this.product.title}">
             <div class="product-item__content">
@@ -47,56 +89,63 @@ class ProductItem {
             <button>Add to Cart</button>
             </div>
             </div>
-            `
-            const addCartButton = prodEl.querySelector('button')
-            addCartButton.addEventListener('click', this.addToCart.bind(this))
-            return prodEl
-    }
-    
+            `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
+  }
 }
 
-class ProductList{
-    products = [
-        new Product(
-            'A Pillow',
-            'https://www.shopmarriott.com/images/products/v2/lrg/Marriott-The-Marriott-Pillow-MAR-108-L_lrg.jpg',
-            'A soft pillow',
-            19.99
-              ),
-        new Product(
-            'Car',
-            'https://c.ndtvimg.com/2019-08/k8519lf8_bugatti-centodieci-unveiled-at-pebble-beach-car-show_625x300_17_August_19.jpg',
-            'Expensive Car',
-            20,000
-        )
-    ]
-    constructor(){}
+class ProductList extends Component {
+  products = [
+    new Product(
+      'A Pillow',
+      'https://www.shopmarriott.com/images/products/v2/lrg/Marriott-The-Marriott-Pillow-MAR-108-L_lrg.jpg',
+      'A soft pillow',
+      19.99
+    ),
+    new Product(
+      'Car',
+      'https://c.ndtvimg.com/2019-08/k8519lf8_bugatti-centodieci-unveiled-at-pebble-beach-car-show_625x300_17_August_19.jpg',
+      'Expensive Car',
+      20,
+      000
+    )
+  ];
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
 
-    render() {
-        
-        const prodList = document.createElement('ul')
-        prodList.className = 'product-list'
-            for (const prod of this.products){
-            const productItem = new ProductItem(prod)
-            const prodEl = productItem.render()
-            prodList.append(prodEl)
-            }
-            return prodList
-        }
+  render() {
+    this.createRootElement('ul', 'product-list', [
+      new ElementAttribute('id', 'prod-list')
+    ]);
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod, 'prod-list');
+      productItem.render();
     }
+  }
+}
 
-    class Shop {
-        render(){
-            const renderHook = document.getElementById('app')
-            const cart = new ShoppingCart()
-            const cartEl = cart.render()
-            const productList = new ProductList()
-            const prodListEl = productList.render()
+class Shop {
+  render() {
+    this.cart = new ShoppingCart('app');
+    this.cart.render();
+    const productList = new ProductList('app');
+    productList.render();
+  }
+}
 
-            renderHook.append(cartEl)
-            renderHook.append(prodListEl)
-        }
-    }
-        
-const shop = new Shop()
-shop.render()
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
